4d29eee4a3973132301b6fb72308cc78202d96b4
Uriel Caminha <urielkindermann@gmail.com>
2019-11-01 22:21:47 -0300

    fix: multiple user bug and heroku

--- telegram-bot.js
+++ telegram-bot.js
@@ -11,15 +11,22 @@ Precisa de ajuda? Digite /help
 const helpMessage = `
 /login usuario senha   - para poder logar no seu moodle
 /presenca   - para ver as porcentagens de presenças sobre sessões anotadas (precisa estar logado)
+
+contato: urielkindermann@gmail.com
 `;
 
 const loginMessage = `
+---------------------------------------------
+
+
 Login registrado com sucesso!
 Para consultar suas presenças use o comando: /presenca
+
+
+---------------------------------------------
 `;
 
-let username;
-let password;
+let login = {};
 
 bot.start((ctx) => ctx.reply(welcomeMessage));
 
@@ -27,29 +34,37 @@ bot.help((ctx) => ctx.reply(helpMessage));
 
 // /login
 bot.hears([/login (.+)/, '/login'], async (ctx) => {
-  
+  let userId = ctx.update.message.from.id;
   let req = ctx.match[1].split(' ');
   if (req.length !== 2)
     return ctx.reply('Digite o comando no padrão: /login usuarioExemplo senhaExemplo');
  
   ctx.deleteMessage();
-  await ctx.reply('----------------------------');
-  await ctx.reply('----------------------------');
-  await ctx.reply('----------------------------');
   ctx.reply(loginMessage);
-  username = req[0];
-  password = req[1];
+
+  console.log(userId + " ------ " + ctx.update.message.from.username);
+  login[userId] = {
+    username: req[0],
+    password: req[1]
+  }
 });
 
 // /presença
 bot.hears('/presenca', async (ctx) => {
-  if (!username || !password)
+  let userId = ctx.update.message.from.id;
+
+  if (!login[userId] || !login[userId].username || !login[userId].password)
     return ctx.reply('Preencha o usuário e a senha, digite /help para mais informações!');
 
   ctx.reply('Computando... Aguarde...');
-  let messages = await script(username, password);
-  await ctx.reply('Porcentagem de presença sobre sessões anotadas:');
-  messages.map((msg) => ctx.reply(msg));
+  try {
+    let messages = await script(login[userId].username, login[userId].password);
+    await ctx.reply('Porcentagem de presença sobre sessões anotadas:');
+    messages.map((msg) => ctx.reply(msg));
+  } catch (err) {
+    console.log(err);
+    ctx.reply('Erro inesperado, verifique seu login, caso o erro persistir contate o suporte');
+  }
   
 });
 
